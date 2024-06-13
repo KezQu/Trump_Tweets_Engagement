854cad7afb9b6bf8fd58d6a3cd90d17da424ee44
Jakub Mikusek <j.mikusek01@gmail.com>
2024-06-13 19:18:51 +0200

    repaired and added more logging

--- word2vec.py
+++ word2vec.py
@@ -2,6 +2,7 @@ import pandas as pd
 # import cupy as np
 import numpy as np
 import re
+import os
 import string
 import scipy as sp
 import matplotlib.pyplot as plt
@@ -65,7 +66,9 @@ def plot(words_df : pd.DataFrame, words = None, labels = True) :
 def stripPunctuation(list_of_sentences : list) :
 	list_of_words = []
 	for text in list_of_sentences :
-		list_of_words.append(np.array([word for word in re.sub('\W', ' ', re.sub("[’']", "", re.sub("http[\S]+", "URL", text, flags=re.U), flags=re.U), flags=re.U).lower().split()]))
+		tmp = np.array([word for word in re.sub('\W', ' ', re.sub("[’']", "", re.sub("http[\S]+", "URL", text, flags=re.U), flags=re.U), flags=re.U).lower().split()])
+		if len(tmp) > 1 :
+			list_of_words.append(tmp)
 	return list_of_words
 
 ################################################################################################################################################################################
@@ -77,8 +80,10 @@ class Word2Vec :
 	NEG_SAMPLING = 0
 	cpu_count = 1
 	list_of_tweets = []
-	def __init__(self, list_of_sentences: list, embedding: int = 5, context_size : int = 2, negative_sampling : int = 3, cpu_count : int = 1) :
+	def __init__(self, list_of_sentences: list = [], embedding: int = 5, context_size : int = 2, negative_sampling : int = 3, cpu_count : int = 1) :
 		self.words_df = pd.DataFrame(columns=["word", "vector", "context"])
+		if len(list_of_sentences) == 0 :
+			return
 		
 		self.EMBEDDING = embedding
 		self.CONTEXT_SIZE = context_size
@@ -93,50 +98,74 @@ class Word2Vec :
 				normalize(np.random.rand(self.EMBEDDING) * 2 - 1),
 				pd.DataFrame(columns=["idx", "label", "vector"])
 				]
-
+		print("core words found:", self.words_df.shape[0], ", continuing to context on", os.getpid())
 		self.FindContext()
 
 ################################################################################################################################################################################
 
-	def __init__(self) :
-		self.words_df = pd.DataFrame(columns=["word", "vector", "context"])
+	def randmizeVectors(self, EMBEDDING) :
+		self.EMBEDDING = EMBEDDING
+		self.words_df["vector"] = [normalize(np.random.rand(self.EMBEDDING) * 2 - 1) for _ in self.words_df.index.values]
+		for i in range(self.words_df.shape[0]) :
+			self.words_df.loc[i, "context"]["vector"] = self.words_df["vector"][self.words_df["context"][i]["idx"]].values
 
 ################################################################################################################################################################################
 
+
 	def partialCtx(self, arr) :
 		for i in arr :
-			ctx_range = []
-			for id, corpus in enumerate(self.list_of_tweets) :
-				for wordIdx in np.where(corpus == self.words_df["word"][i])[0] :
-					window = [self.CONTEXT_SIZE,self.CONTEXT_SIZE]
-					if wordIdx < window[0] : window[0] = wordIdx
-					if (len(corpus) - 1) - wordIdx < window[1] : window[1] = (len(corpus) - 1) - wordIdx
-					if window[0] > 0 : 
-						ctx_range.append([(id, i) for i in range(wordIdx - window[0], wordIdx)])
-					if window[1] > 0 : 
-						ctx_range.append([(id, i) for i in range(wordIdx + 1, wordIdx + window[1] + 1)])
-			ctx_range = np.concatenate(ctx_range)
-			
-			context = self.words_df.loc[self.words_df["word"].isin([self.list_of_tweets[pair[0]][pair[1]] for pair in ctx_range])]
-			self.words_df["context"][i]["idx"] = context["word"].index.values
-			self.words_df["context"][i]["label"] = [1 for _ in range(context.shape[0])]
-			self.words_df["context"][i]["vector"] = context["vector"].values
-			self.words_df["context"][i].index = context["word"].values
-			
-			for _ in range(self.NEG_SAMPLING * self.words_df["context"][i].shape[0]) :
-				try:
-					sample_idx = random.choice([idx for idx in self.words_df.index.values if idx not in self.words_df["context"][i]["idx"].values])
-					self.words_df["context"][i].loc[self.words_df["word"][sample_idx]] = [sample_idx, 0, self.words_df["vector"][sample_idx]]
+			try:
+				ctx_range = []
+				print("Finding context -- Process", os.getpid(), "%3.1f" % ((i - arr[0]) / len(arr) * 100), "%", "completed")
+				for id, corpus in enumerate(self.list_of_tweets) :
+					for wordIdx in np.where(corpus == self.words_df["word"][i])[0] :
+						window = [self.CONTEXT_SIZE,self.CONTEXT_SIZE]
+						if wordIdx < window[0] : window[0] = wordIdx
+						if (len(corpus) - 1) - wordIdx < window[1] : window[1] = (len(corpus) - 1) - wordIdx
+						if window[0] > 0 : 
+							ctx_range.append([(id, i) for i in range(wordIdx - window[0], wordIdx)])
+						if window[1] > 0 : 
+							ctx_range.append([(id, i) for i in range(wordIdx + 1, wordIdx + window[1] + 1)])
+				try :
+					ctx_range = np.concatenate(ctx_range)
 				except Exception as e :
-					print("unable to find more negative samples for: \"" + self.words_df["word"][i] + "\" skipping...")
-					break
+					with open("./DEBUG/error_concat" + str(i) + ".txt", "w") as f:
+						f.write(str(i) + " " + str(self.words_df["word"][i]) + " " + str(e))
+
+				context = self.words_df.loc[self.words_df["word"].isin([self.list_of_tweets[pair[0]][pair[1]] for pair in ctx_range])]
+				self.words_df["context"][i]["idx"] = context["word"].index.values
+				self.words_df["context"][i]["label"] = [1 for _ in range(context.shape[0])]
+				self.words_df["context"][i]["vector"] = context["vector"].values
+				self.words_df["context"][i].index = context["word"].values
+			except Exception as e:
+				with open("./DEBUG/error_ctx" + str(i) + ".txt", "w") as f:
+					f.write(str(e) + " " + str(i))
+
+		print("Falling for searching context on", os.getpid())
+		for i in arr :
+			try:
+				print("Finding neg_sampling -- Process", os.getpid(), "%3.1f" % ((i - arr[0]) / len(arr) * 100), "%", "completed")
+				to_choose_from = [idx for idx in self.words_df.index.values if idx not in self.words_df["context"][i]["idx"].values]
+				for _ in range(self.NEG_SAMPLING * self.words_df["context"][i].shape[0]) :
+					try:
+						sample_idx = random.choice(to_choose_from)
+						to_choose_from.remove(sample_idx)
+						self.words_df["context"][i].loc[self.words_df["word"][sample_idx]] = [sample_idx, 0, self.words_df["vector"][sample_idx]]
+					except Exception as e :
+						print("unable to find more negative samples for: \"" + self.words_df["word"][i] + "\" skipping...")
+						break
+			except Exception as e:
+				with open("./DEBUG/error_neg" + str(i) + ".txt", "w") as f:
+					f.write(str(e) + " " + str(i))
+		print("Finished batch", arr)
 		return self.words_df["context"]
 
 ################################################################################################################################################################################
 
 	def FindContext(self) :
 		thread_pool = mp.Pool(processes=self.cpu_count)
-		self.words_df["context"] = [row for row in np.concatenate(thread_pool.map(self.partialCtx, np.array_split(np.arange(self.words_df.shape[0]), self.cpu_count))) if row.size > 0]
+		contexts = thread_pool.map(self.partialCtx, np.array_split(np.arange(self.words_df.shape[0]), self.cpu_count))
+		self.words_df["context"] = [row for row in np.concatenate(contexts) if row.size > 0]
 		thread_pool.close()
 
 ################################################################################################################################################################################
@@ -148,7 +177,7 @@ class Word2Vec :
 			file.write(str(self.words_df.loc[i]["vector"]) + "\n")
 			file.write("-----------------------------------------------\n")
 			for j in self.words_df.loc[i]["context"].index.values:
-				file.write(j + ", " + str(self.words_df.loc[i]["context"].loc[j]["idx"]) + ", " + str(self.words_df.loc[i]["context"].loc[j]["label"]) + "\n")
+				file.write(str(j) + ", " + str(self.words_df.loc[i]["context"].loc[j]["idx"]) + ", " + str(self.words_df.loc[i]["context"].loc[j]["label"]) + "\n")
 			file.write("-----------------------------------------------\n")
 			
 		file.close()
@@ -187,10 +216,7 @@ class Word2Vec :
 	def update(self, processed_df : pd.DataFrame) :
 		copy = processed_df.copy()
 
-		# thread_pool = mp.Pool(processes=self.cpu_count)
 		# copy["vector"] = thread_pool.map(self.calcBias, np.array_split(np.arange(self.words_df.shape[0]), 2))[0]
-		# thread_pool.close()
-		
 		copy["vector"] = self.calcBias(np.arange(self.words_df.shape[0]))
 		
 		for i in range(processed_df.shape[0]) :
@@ -203,6 +229,7 @@ class Word2Vec :
 	def train(self, learning_rate : float, max_steps: int, early_stop : float = None, draw_middle_results = False, words = None) :
 		self.learning_rate = learning_rate
 		self.processed_df = self.words_df.copy()
+		# thread_pool = mp.Pool(processes=self.cpu_count)
 		word2vec_list = [(np.vstack(self.processed_df["vector"].values), accuracy(score(self.processed_df)))]
 
 		print("Processing: %3d" % int(0 / max_steps * 100) + "%" + " --- accuracy in current step: %4.5f" % accuracy(score(self.processed_df)))
@@ -213,8 +240,8 @@ class Word2Vec :
 			self.processed_df = self.update(self.processed_df)
 			word2vec_list.append((np.vstack(self.processed_df["vector"].values), accuracy(score(self.processed_df))))
 			
+			print("Processing: %3.1f" % (float(i + 1) / max_steps * 100) + "%" + " --- accuracy in current step: %4.5f" % accuracy(score(self.processed_df)))
 			if (i + 1) % int(max_steps / 10) == 0 :
-				print("Processing: %3d" % int((i + 1) / max_steps * 100) + "%" + " --- accuracy in current step: %4.5f" % accuracy(score(self.processed_df)))
 				if draw_middle_results:
 					plot(self.processed_df, words=words)
 			if early_stop is not None :
